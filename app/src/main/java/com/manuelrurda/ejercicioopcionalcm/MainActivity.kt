d76ec6ace713a0bb24197892d8c7e365cf42d7a8
Manuel Rodriguez Urdapilelta <manuelrurda@gmail.com>
2024-11-08 10:42:04 -0600

    Initial commit

---
+++ app/src/main/java/com/manuelrurda/ejercicioopcionalcm/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.manuelrurda.ejercicioopcionalcm
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.manuelrurda.ejercicioopcionalcm.ui.theme.EjercicioOpcionalCMTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            EjercicioOpcionalCMTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    EjercicioOpcionalCMTheme {
+        Greeting("Android")
+    }
+}
